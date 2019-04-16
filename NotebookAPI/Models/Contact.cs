076f57fd2a06e2897935cd64465518a6f404f102
MikhailovAlexander <vestroy@gmail.com>
2019-04-17 01:40:23 +0500

    Initial commit

---
+++ NotebookAPI/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace NotebookAPI.Models
+{
+    public class Contact : BaseEntity
+    {
+        public int PersonId { get; set; }
+        public int ContactTypeId { get; set; }
+        public string Value { get; set; }
+
+        [JsonIgnore]
+        public virtual Person Person { get; set; }
+        [JsonIgnore]
+        public virtual ContactType ContactType { get; set; }
+    }
+}
